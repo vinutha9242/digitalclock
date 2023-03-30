8c108a48fdc3d4d4f968be4b70fbac15a374fd9d
vinutha9242 <vinuthanreddy@gmail.com>
2023-03-30 20:28:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const hourEl = document.getElementById("hour");
+const minuiteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
+const ampmEl = document.getElementById("ampm");
+
+function updateTime(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if(h>12){
+        h=h-12;
+        ampm ="PM";
+    }
+
+    h=h<10?"0"+h:h;
+    m=m<10?"0"+m:m;
+    s=s<10?"0"+s:s;
+
+    hourEl.innerText = h;
+    minuiteEl.innerText=m;
+    secondEl.innerText=s;
+    ampmEl.innerText= ampm;
+
+    setTimeout(() => {
+        updateTime();
+        
+    }, 1000);
+
+}
+
+updateTime();
